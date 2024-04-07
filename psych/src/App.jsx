653dd861badb5aa1bc978d96c256ed7740305dd7
Anshal543 <sherhasan543@gmail.com>
2024-04-07 23:26:00 +0500

    landing page

---
+++ psych/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
+
+
+const App = () => {
+
+
+  return (
+    <div>
+
+    <Nav />
+    <About />
+    </div>
+  )
+};
+
+export default App;
