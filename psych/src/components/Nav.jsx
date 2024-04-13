5990ac6010d2621ad6c2aee387120cf1a4adbb54
Anshal543 <sherhasan543@gmail.com>
2024-04-13 17:13:56 +0500

    landing page

--- psych/src/components/Nav.jsx
+++ psych/src/components/Nav.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import '../components/Nav.css'
+import React from "react";
+import "../components/Nav.css";
 
 const Nav = () => {
   return (
     <div>
-        <div className='intro'>
-            <h2>Walter Scott Newsom, PH.D.</h2>
-            <p className='para'>Licensed Psychologist</p>
+      <div className="intro">
+        <h2>Walter Scott Newsom, PH.D.</h2>
+        <p className="para">Licensed Psychologist</p>
+      </div>
+      <div className="nav-img">
+        <ul className="list">
+          <li className="nav-li">Home</li>
+          <li className="nav-li">About Me</li>
+          <li className="nav-li">Services</li>
+          <li className="nav-li">Client Info</li>
+          <li className="nav-li">Request an Appointment</li>
+          <li className="nav-li">Contact</li>
+        </ul>
+        <div className="text-outer-div">
+          <h1 className="nav-text">
+            The Best PSY.D Program in Clinical Psychologist
+          </h1>
         </div>
-        <div className='nav-img'>
-            <ul className='list'>
-                <li className='nav-li'>Home</li>
-                <li className='nav-li'>About Me</li>
-                <li className='nav-li'>Services</li>
-                <li className='nav-li'>Client Info</li>
-                <li className='nav-li'>Request an Appointment</li>
-                <li className='nav-li'>Contact</li>
-            </ul>
-            <div className='text-outer-div'>
-                <h1 className='nav-text'>The Best PSY.D Program in Clinical Psychologist</h1>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
